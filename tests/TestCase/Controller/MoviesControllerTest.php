a0341901204e5faf9a38681aa53b579df7a4d577
Mourierh <heloise.mourier@etud.u-picardie.fr>
2017-11-16 18:46:50 +0000

    *Revert "a"
    This reverts commit 06bc23f6744796b138f7583a89a9cac6cfbdd5fc.

--- tests/TestCase/Controller/MoviesControllerTest.php
+++ tests/TestCase/Controller/MoviesControllerTest.php
@@ -2,6 +2,7 @@
 namespace App\Test\TestCase\Controller;
 
 use App\Controller\MoviesController;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\IntegrationTestCase;
 
 /**
@@ -9,6 +10,12 @@ use Cake\TestSuite\IntegrationTestCase;
  */
 class MoviesControllerTest extends IntegrationTestCase
 {
+
+    private static $movie = [
+        'name' => 'Mega Shark',
+        'description' => 'un film culte !',
+        'duration' => 120
+    ];
 
     /**
      * Fixtures
@@ -26,7 +33,8 @@ class MoviesControllerTest extends IntegrationTestCase
      */
     public function testIndex()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('movies');
+        $this->assertResponseSuccess();
     }
 
     /**
@@ -36,7 +44,8 @@ class MoviesControllerTest extends IntegrationTestCase
      */
     public function testView()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('movies/view/1');
+        $this->assertResponseSuccess();
     }
 
     /**
@@ -46,7 +55,27 @@ class MoviesControllerTest extends IntegrationTestCase
      */
     public function testAdd()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('movies/add');
+        $this->assertResponseSuccess();
+    }
+
+    public function testAddCheckRedirection()
+    {
+        $this->post('/movies/add', self::$movie);
+
+        $this->assertResponseSuccess();
+        $this->assertRedirect(['controller' => 'movies', 'action' => 'index']);
+    }
+
+    public function testAddCheckDatabase()
+    {
+        $this->post('/movies/add', self::$movie);
+
+        $this->assertResponseSuccess();
+
+        $movies = TableRegistry::get('Movies');
+        $query = $movies->find()->where(['name' => 'Mega Shark']);
+        $this->assertEquals(1, $query->count());
     }
 
     /**
@@ -56,7 +85,29 @@ class MoviesControllerTest extends IntegrationTestCase
      */
     public function testEdit()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('movies/edit/1');
+        $this->assertResponseSuccess();
+    }
+
+    public function testEditCheckRedirectionOnSuccess()
+    {
+        $this->post('/movies/edit/1', []);
+
+        $this->assertResponseSuccess();
+        $this->assertRedirect(['controller' => 'movies', 'action' => 'index']);
+    }
+
+    public function testEditCheckModificationInDatabase()
+    {
+        $movies = TableRegistry::get('Movies');
+
+        $movie = [
+            'name' => 'Titre modifié'
+        ];
+        $this->post('/movies/edit/1', $movie);
+
+        $movieAfterEdit = $movies->get(1);
+        $this->assertEquals('Titre modifié', $movieAfterEdit->name);
     }
 
     /**
@@ -66,6 +117,7 @@ class MoviesControllerTest extends IntegrationTestCase
      */
     public function testDelete()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->delete('movies/delete/1');
+        $this->assertResponseSuccess();
     }
 }
